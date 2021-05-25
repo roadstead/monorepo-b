a867e49254dd08b664ad09f9b9fd569a046eb5de
Jenkins <kf@skm.dk>
2021-05-25 07:48:24 +0200

    now its on - add additional dep-b, with transitive deps

---
+++ deployable-b/test/deployable_b/core_test.clj
@@ -0,0 +1,7 @@
+(ns deployable-b.core-test
+  (:require [clojure.test :refer :all]
+            [deployable-b.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
---
+++ lib-b/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lib-b
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
