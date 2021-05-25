d1713222f4101c0ecdd0d2ba9c0f5d247c1865d6
Jenkins <kf@skm.dk>
2021-05-25 09:44:12 +0200

    ready for a release

--- deployable-b/project.clj
+++ deployable-b/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [deployable-a "0.1.0-SNAPSHOT"]]
+                 [deployable-a "0.1.0-9-0b263fd"]]
   :main ^:skip-aot deployable-b.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
